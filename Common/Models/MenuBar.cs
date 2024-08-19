1eb474823ecfbf52a4fcbc8dbc7b52fed83155f8
yuke zhou <1976475277@qq.com>
2024-08-19 18:26:50 +0800

    Notetest

---
+++ Common/Models/MenuBar.cs
@@ -0,0 +1,47 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Note.Common.Models
+{
+	/// <summary>
+	/// 导航系统实体类
+	/// </summary>
+    public class MenuBar:BindableBase
+    {
+		/// <summary>
+		/// 菜单图标
+		/// </summary>
+		private String icon;
+
+		public String Icon
+		{
+			get { return icon; }
+			set { icon = value; }
+		}
+		/// <summary>
+		/// 菜单名称
+		/// </summary>
+		private string title;
+
+		public string Title
+		{
+			get { return title; }
+			set { title = value; }
+		}
+        /// <summary>
+        /// 菜单命名空间
+        /// </summary>
+        private String nameSpace;
+
+		public String NameSapce
+		{
+			get { return nameSpace; }
+			set { nameSpace = value; }
+		}
+
+	}
+}
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,13 +1,37 @@
-﻿using System;
+﻿using Note.Common.Models;
+using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace Note.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel:BindableBase
     {
+        public  MainViewModel()
+        {
+            MenuBars = new ObservableCollection<MenuBar>();
 
+        }
+
+        private ObservableCollection<MenuBar> menuBars;
+        public ObservableCollection<MenuBar> MenuBars
+        {
+            get { return menuBars; }
+            set { menuBars = value;RaisePropertyChanged();}
+        }
+
+        void CreateMenuBar()
+        {
+            MenuBars.Add(new MenuBar() { Icon = "Home",Title = "首页",NameSapce = "IndexView"});
+            MenuBars.Add(new MenuBar() { Icon = "NotebookOutline", Title = "待办事项", NameSapce = "ToDoView" });
+            MenuBars.Add(new MenuBar() { Icon = "NotebookPlus", Title = "备忘录", NameSapce = "MemoView" });
+            MenuBars.Add(new MenuBar() { Icon = "Cog", Title = "设置", NameSapce = "SettingView" });
+        }
     }
 }
